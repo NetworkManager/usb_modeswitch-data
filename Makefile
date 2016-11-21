cf92c100aae8b1eeb0b928e53ae14a1d3c9e4a94
Lubomir Rintel <lkundrak@v3.sk>
2016-11-21 13:54:59 +0100

    Import usb-modeswitch-data-20160803
    http://www.draisberghof.de/usb_modeswitch/usb-modeswitch-data-20160803.tar.bz2

---
+++ Makefile
@@ -0,0 +1,45 @@
+PROG        = usb-modeswitch-data
+VERS        = 20160803
+RM          = /bin/rm -f
+PREFIX      = $(DESTDIR)/usr
+ETCDIR      = $(DESTDIR)/etc
+RULESDIR    = $(DESTDIR)/lib/udev/rules.d
+
+
+.PHONY:     clean
+
+all: 40-usb_modeswitch.rules
+
+clean:
+	$(RM) 40-usb_modeswitch.rules
+
+install: all files-install db-install
+
+install-packed: files-install db-install-packed
+
+files-install: 
+	install -d $(PREFIX)/share/usb_modeswitch
+	install -d $(ETCDIR)/usb_modeswitch.d
+	install -D --mode=644 40-usb_modeswitch.rules $(RULESDIR)/40-usb_modeswitch.rules
+
+40-usb_modeswitch.rules:
+	./gen-rules.tcl
+
+db-install: files-install
+	install --mode=644 -t $(PREFIX)/share/usb_modeswitch ./usb_modeswitch.d/*
+
+db-install-packed:
+	@# Create a compressed tar without gzip timestamp, so tar.gz
+	@# differs only if content is different
+	cd ./usb_modeswitch.d; find * -print0 | LC_ALL=C sort -z | tar --no-recursion --null --files-from=- --mode=go=rX,u+rw,a-s -cf ../configPack.tar
+	gzip -f9n ./configPack.tar
+	install --mode=644 -t $(PREFIX)/share/usb_modeswitch ./configPack.tar.gz
+	rm -f ./configPack.tar.gz
+
+uninstall: files-uninstall
+
+files-uninstall:
+	$(RM) $(RULESDIR)/40-usb_modeswitch.rules
+	$(RM) -R $(PREFIX)/share/usb_modeswitch
+
+.PHONY:    clean install uninstall
---
+++ gen-rules.tcl
@@ -0,0 +1,86 @@
+#!/usr/bin/env tclsh
+#
+# Usage: gen-rules.tcl [--set-version <date>]
+#
+# <date> should be in the form YYYYMMDD
+#
+# Config files are expected to be in subfolder "usb_modeswitch.d"
+#
+# A config file is expected to have one comment line containing
+# a model name or other concise device specifications
+
+
+# Default version string
+set version "20160803"
+
+# Devices excluded from Huawei catch-all rule
+set x_huaweiList {12d1:1573 12d1:15c1}
+
+if {[lindex $argv 0] == "--set-version" && [regexp {\d\d\d\d\d\d\d\d} [lindex $argv 1]]} {
+	set version [lindex $argv 1]
+}
+
+set template {ATTR{idVendor}=="+##+", ATTR{idProduct}=="#++#", RUN+="usb_modeswitch '%b/%k'"}
+
+if {![file isdirectory usb_modeswitch.d]} {
+	puts "No \"usb_modeswitch.d\" subfolder found"
+	exit
+}
+
+set filelist [lsort [glob -nocomplain ./usb_modeswitch.d/*]]
+if {[llength $filelist] == 0} {
+	puts "The \"usb_modeswitch.d\" subfolder is empty"
+	exit
+}
+
+set wc [open "40-usb_modeswitch.rules" w]
+
+# Writing file header with given version
+
+puts -nonewline $wc {# Part of usb-modeswitch-data, version }
+puts $wc $version
+puts $wc {#
+# Works with usb_modeswitch versions >= 2.4.0 (extension of StandardEject)
+#
+ACTION!="add|change", GOTO="modeswitch_rules_end"
+
+# Adds a symlink "gsmmodem[n]" to the lowest ttyUSB port with interrupt
+# transfer; checked against a list of known modems, or else no action
+KERNEL=="ttyUSB*", ATTRS{bNumConfigurations}=="*", PROGRAM="usb_modeswitch --symlink-name %p %s{idVendor} %s{idProduct} %E{PRODUCT}", SYMLINK+="%c"
+
+SUBSYSTEM!="usb", ACTION!="add",, GOTO="modeswitch_rules_end"
+
+# Generic entry for most Huawei devices, excluding Android phones
+ATTRS{idVendor}=="12d1", ATTRS{manufacturer}!="Android", ATTR{bInterfaceNumber}=="00", ATTR{bInterfaceClass}=="08", RUN+="usb_modeswitch '%b/%k'"}
+
+set vendorList ""
+set dvid ""
+
+foreach idfile $filelist {
+	if {![regexp -nocase {./([0-9A-F]{4}:[0-9A-F]{4})} $idfile d id]} {continue}
+	if {[regexp -nocase {^12d1:} $id] && [lsearch $x_huaweiList $id] == -1} {continue}
+	if [info exists entry($id)] {
+		append entry($id) ", "
+	}
+	set rc [open $idfile r]
+	set buffer [read $rc]
+	close $rc
+	foreach line [split $buffer \n] {
+		set comment {}
+		regexp {# (.*)} $line d comment
+		if {[string length $comment] > 0} {
+			append entry($id) [string trim $comment]
+			break
+		}
+	}
+}
+foreach id_entry [lsort [array names entry]] {
+	set id [split $id_entry :]
+	set rule [regsub {\+##\+} $template [lindex $id 0]]
+	set rule [regsub {#\+\+#} $rule [lindex $id 1]]
+	puts $wc "\n# $entry($id_entry)\n$rule"
+}
+
+puts $wc {
+LABEL="modeswitch_rules_end"}
+close $wc
